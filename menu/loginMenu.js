3ad50f9cdfa6525a2ef24dfe632f1a6681a699c6
coffeegoblins <coffeegoblins@gmail.com>
2014-10-11 12:51:42 -0500

    Updating Server URL

--- menu/loginMenu.js
+++ menu/loginMenu.js
@@ -1,6 +1,6 @@
 define(['text!menu/loginMenu.html', 'menu/menuNavigator', 'lib/socket.io'], function (Template, MenuNavigator, io)
 {
-    var SERVER_URL = 'http://nodejs-coffeegoblins.rhcloud.com:8000';
+    var SERVER_URL = 'http://nodejs-coffeegoblins.rhcloud.com:8080';
     // var SERVER_URL = 'http://127.0.0.1:3000';
 
     return {
